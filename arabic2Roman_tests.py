eb04b7745cacd72d48b1cad6b27fe9dcd570d774
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-01-27 10:13:49 -0500

    max and empty test added

--- arabic2Roman_tests.py
+++ arabic2Roman_tests.py
@@ -2,3 +2,9 @@ from arabic2Roman import arabic2Roman
 
 def test1():
     assert arabic2Roman(33) == "XXXIII"
+
+def empty_test():
+    assert arabic2Roman() == ""
+
+def max_num():
+    assert arabic2Roman(3999) == "MMMCMXCIX"
