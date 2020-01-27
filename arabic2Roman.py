d583fe9f9ea0071dbe09fabb1a7a1a623dda8a09
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-01-26 22:09:04 -0500

    first iteration, no error checking

--- arabic2Roman.py
+++ arabic2Roman.py
@@ -1,24 +1,12 @@
-#figure out how to take user input 
-
-#use modulus for different levels of arabic numerals
-#   M for 1000
-#   C for 100
-#   L for 50
-#   X for 10
-#   V for 5
-#   I for 1
-#special case: 4 (IV), 9(IX)
-
-
-#start with empty string that we append on to the end each time
-#write it recursively?
+#Brian Macomber - U25993688
+#EC500 - Assignment 1
+#Sources referenced: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
 
 
 
 def main():
     userInput = input("Enter an arabic numeral: ")
     romanString = ""
-    #print(type(userIn)
     finalRomanString = arabic2Roman(int(userInput),romanString)
     print("The number %d in roman numerals is %s." % (int(userInput),finalRomanString))
 
@@ -26,44 +14,21 @@ def main():
 
 
 def arabic2Roman(userInput, romanString):
-    if userInput == 0:
-        romanString = romanString + "0"
-        return romanString
-    elif userInput == 1:
-        romanString = romanString + "I"
-        return romanString
-    # elif userInput < 4:
-    #     userInput = userInput - 1
-    #     print(userInput)
-    #     romanString = romanString + "I"
-    #     arabic2Roman(userInput,romanString)
-    # elif userInput == 4:
-    #     romanString = romanString + "IV"
-    #     return romanString 
-    # if (userInput < 100) and (userInput >= 50):
-    #     userInput % 50
-    #     romanString + "L"
-    #     arabic2Roman(userInput,romanString)
+    romans = ["M", "CM", "D", "CD",
+              "C", "XC", "L", "XL", "X",
+              "IX", "V", "IV", "I"
+    ]
+    ints = [1000, 900, 500, 400,
+            100, 90, 50, 40, 10,
+            9, 5, 4, 1
+    ]
 
-    # if userInput < 50 and userInput >= 10:
-    #     userInput % 10
-    #     romanString + "X"
-    #     arabic2Roman(userInput,romanString)
-
-    # if userInput < 100 and userInput >= 50:
-    #     userInput % 50
-    #     romanString + "L"
-    #     arabic2Roman(userInput,romanString)
-
-        
-    # if userInput < 1000 and userInput >= 100:
-    #     userInput % 100
-    #     romanString + "C"
-    #     arabic2Roman(userInput,romanString)
-    # else:
-    #     userInput%1000
-    #     romanString + "M"
-    #     arabic2Roman(userInput,romanString)
-
+    romanNumString = ""
+    for i in range(len(ints)):
+        freq = int(userInput/ints[i])
+        romanNumString = romanNumString + (freq*romans[i])
+        userInput-=(freq*ints[i])
+    
+    return romanNumString
 
 main()
