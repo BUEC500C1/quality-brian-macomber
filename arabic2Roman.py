2653ad3fe41d373295f07f9c8d0c4e01dc39e032
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-01-27 21:46:46 -0500

    fixed all flake8 issues

--- arabic2Roman.py
+++ arabic2Roman.py
@@ -1,20 +1,25 @@
 # Brian Macomber - U25993688
 # EC500 - Assignment 1
-# Sources referenced: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
+# Sources referenced:
+# https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
 
 
 def arabic2Roman(userInput):
 
     # catching empty and corner cases
-    if not isinstance(userInput,int):
+
+    if not isinstance(userInput, int):
         print("The input was not an integer, please enter an integer")
         return ""
+
     if userInput == "":
         print("The input was empty, please enter an arabic numeral")
         return ""
+
     if userInput > 3999 or userInput < 0:
         print("The input is out of range, enter a num between 0 and 3999")
         return ""
+
     if userInput == 0:
         return 0
 
@@ -22,17 +27,16 @@ def arabic2Roman(userInput):
 
     romans = ["M", "CM", "D", "CD",
               "C", "XC", "L", "XL", "X",
-              "IX", "V", "IV", "I"
-    ]
+              "IX", "V", "IV", "I"]
+
     ints = [1000, 900, 500, 400,
             100, 90, 50, 40, 10,
             9, 5, 4, 1
-    ]
+            ]
 
     romanNumString = ""
     for i in range(len(ints)):
         freq = int(userInput/ints[i])
         romanNumString = romanNumString + (freq*romans[i])
-        userInput-=(freq*ints[i])
-    
+        userInput -= (freq * ints[i])
     return romanNumString
